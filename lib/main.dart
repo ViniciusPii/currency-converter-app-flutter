4232117f0c6933684b9b4b83b004fc8a48c8ddc7
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-07-07 18:58:19 -0300

    Ajustes no método http, e inicio da estilização do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MaterialApp(
-    home: Container(),
+    debugShowCheckedModeBanner: false,
+    home: Home(),
   ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "Conversor de Moedas"
+        ),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(),
+    );
+  }
+}
+
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
 
 
