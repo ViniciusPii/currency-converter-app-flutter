447e6aaf9fe5ee49ce99a042cd59a1b5039fdcd8
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-07-07 19:45:01 -0300

    Criação do Layout

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,20 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Home(),
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(
+          primaryColor: Colors.green,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.green)),
+          )),
+    ),
+  );
 }
 
 class Home extends StatefulWidget {
@@ -19,6 +29,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _dolar;
+  var _euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,8 +69,71 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.white,
+                _dolar = snap.data["results"]["currencies"]["USD"]["buy"];
+                _euro = snap.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Icon(
+                          Icons.monetization_on,
+                          size: 100,
+                          color: Colors.green,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.green,
+                              ),
+                            ),
+                            prefix: Text("R\$ "),
+                          ),
+                          style: TextStyle(color: Colors.black54, fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "Dólares",
+                            labelStyle: TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.green,
+                              ),
+                            ),
+                            prefix: Text("US\$ "),
+                          ),
+                          style: TextStyle(color: Colors.black54, fontSize: 20),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "Euros",
+                            labelStyle: TextStyle(color: Colors.green),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.green,
+                              ),
+                            ),
+                            prefix: Text("€ "),
+                          ),
+                          style: TextStyle(color: Colors.black54, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }
           }
