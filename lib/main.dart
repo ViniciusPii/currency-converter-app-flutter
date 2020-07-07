511f4c1c45327bb368eb5f9eacbd4f443a67d058
Vinicius <vinicius@iMac-de-Vinicius.local>
2020-07-07 19:08:13 -0300

    Configuração do FutureBuilder e construção dos erros

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'dart:convert';
 const request = "https://api.hgbrasil.com/finance";
 
 void main() async {
-
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
@@ -25,20 +24,50 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: Text(
-          "Conversor de Moedas"
-        ),
+        title: Text("Conversor de Moedas"),
         centerTitle: true,
       ),
-      body: FutureBuilder(),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snap) {
+          switch (snap.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carragando Dados",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snap.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao carregar dados!",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.white,
+                );
+              }
+          }
+        },
+      ),
     );
   }
 }
-
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
-
-
